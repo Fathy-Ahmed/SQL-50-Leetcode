639523c1853d3003830242d32267f500406a6c29
Fathy-Ahmed <fathyahmed42205392@gmail.com>
2025-10-08 08:09:08 +0300

    add problem number 1731

---
+++ 1731. The Number of Employees Which Report to Each Employee.sql
@@ -0,0 +1,11 @@
+select 
+    e1.reports_to as employee_id,
+    e2.name, 
+    count(e1.employee_id) as reports_count,
+    round(avg(e1.age*1.0), 0) as average_age
+
+from Employees e1
+join Employees e2 
+    on e1.reports_to = e2.employee_id
+group by e1.reports_to, e2.name
+order by e1.reports_to
