0f5a21167ccc3cfb4906c48bccbbaf6cacfe61d3
Fathy-Ahmed <fathyahmed42205392@gmail.com>
2025-10-24 16:26:33 +0300

    add problem number 1327

---
+++ 1327. List the Products Ordered in a Period.sql
@@ -0,0 +1,6 @@
+select p.product_name, sum(o.unit) as unit
+from Products p
+join Orders o on o.product_id = p.product_id
+where o.order_date like '2020-02-%'
+group by p.product_name
+having sum(o.unit) >= 100;
